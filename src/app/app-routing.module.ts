a4f0b67e11c5cd8f301cfa11239f142bb111f96f
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-08 17:09:35 -0300

    refactor(app): register CarbonFootprint module and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'carbon-footprint',
+    loadChildren: () =>
+      import('./features/carbon-footprint/carbon-footprint.module').then(
+        (m) => m.CarbonFootprintModule
+      ),
+  },
+  { path: '**', redirectTo: 'carbon-footprint' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
